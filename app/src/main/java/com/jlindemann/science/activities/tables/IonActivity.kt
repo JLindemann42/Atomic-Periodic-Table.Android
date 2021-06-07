0c7a01cbc0936d2a16c31e7d5fe22726e7badb2b
Jonatan Lindemann <46776594+JLindemann42@users.noreply.github.com>
2021-06-07 22:20:46 +0200

    1.4.0-Build.54-dev4
    - Final ionization table
    - Added detail ionization view
    - Fixed chip corners that weren't round
    - Added page indicators

--- app/src/main/java/com/jlindemann/science/activities/tables/IonActivity.kt
+++ app/src/main/java/com/jlindemann/science/activities/tables/IonActivity.kt
@@ -9,11 +9,14 @@ import android.text.TextWatcher
 import android.view.View
 import android.view.ViewGroup
 import android.view.inputmethod.InputMethodManager
+import android.widget.TextView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.jlindemann.science.R
 import com.jlindemann.science.activities.BaseActivity
+import com.jlindemann.science.adapter.EquationsAdapter
 import com.jlindemann.science.adapter.IonAdapter
+import com.jlindemann.science.model.Equation
 import com.jlindemann.science.model.Ion
 import com.jlindemann.science.model.IonModel
 import com.jlindemann.science.preferences.ThemePreference
@@ -21,8 +24,11 @@ import com.jlindemann.science.utils.Utils
 import kotlinx.android.synthetic.main.activity_dictionary.search_btn
 import kotlinx.android.synthetic.main.activity_dictionary.title_box
 import kotlinx.android.synthetic.main.activity_electrode.*
+import kotlinx.android.synthetic.main.activity_equations.*
 import kotlinx.android.synthetic.main.activity_ion.*
 import kotlinx.android.synthetic.main.electrode_list_item.*
+import kotlinx.android.synthetic.main.ion_details.*
+import kotlinx.android.synthetic.main.row_ions_list.*
 import org.json.JSONArray
 import org.json.JSONObject
 import java.io.IOException
@@ -30,9 +36,7 @@ import java.io.InputStream
 import java.util.*
 import kotlin.collections.ArrayList
 
-
-
-class IonActivity : BaseActivity() {
+class IonActivity : BaseActivity(), IonAdapter.OnIonClickListener {
     private var ionList = ArrayList<Ion>()
     var mAdapter = IonAdapter(ionList, this, this)
 
@@ -40,7 +44,6 @@ class IonActivity : BaseActivity() {
         super.onCreate(savedInstanceState)
         val themePreference = ThemePreference(this)
         val themePrefValue = themePreference.getValue()
-
         if (themePrefValue == 100) {
             when (resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK) {
                 Configuration.UI_MODE_NIGHT_NO -> { setTheme(R.style.AppTheme) }
@@ -54,18 +57,49 @@ class IonActivity : BaseActivity() {
         recyclerView()
         clickSearch()
         view_ion.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-        back_btn_ion.setOnClickListener {
-            this.onBackPressed()
+        detail_background_ion.setOnClickListener { Utils.fadeOutAnim(ion_detail, 300) }
+        back_btn_ion.setOnClickListener { this.onBackPressed() }
+    }
+
+    override fun ionClickListener(item: Ion, position: Int) {
+        if (item.count > 1) {
+            Utils.fadeInAnim(ion_detail, 300)
+            ion_detail_title.text = ((item.name).capitalize() + " " + "ionization")
+            var jsonString : String? = null
+            val ext = ".json"
+            val element = item.name
+            val ElementJson: String? = "$element$ext"
+            val inputStream: InputStream = assets.open(ElementJson.toString())
+            jsonString = inputStream.bufferedReader().use { it.readText() }
+
+            val jsonArray = JSONArray(jsonString)
+            val jsonObject: JSONObject = jsonArray.getJSONObject(0)
+            for (i in 1..item.count) {
+                val text = "element_ionization_energy"
+                val add = i.toString()
+                val final = (text + add)
+                val ionization = jsonObject.optString(final, "---")
+                val extText = i.toString()
+                val name = "ion_text_"
+                val eView = "$name$extText"
+                val iText = findViewById<TextView>(resources.getIdentifier(eView, "id", packageName))
+                val dot ="."
+                val space = " "
+                iText.text = "$i$dot$space$ionization"
+                iText.visibility = View.VISIBLE
+            }
+            for (i in (item.count+1)..30) {
+                val extText = i.toString()
+                val name = ("ion_text_")
+                val eView = "$name$extText"
+                val iText = findViewById<TextView>(resources.getIdentifier(eView, "id", packageName))
+                iText.visibility = View.GONE
+            }
         }
     }
 
     override fun onApplySystemInsets(top: Int, bottom: Int, left: Int, right: Int) {
-        ion_view.setPadding(
-            0,
-            resources.getDimensionPixelSize(R.dimen.title_bar) + resources.getDimensionPixelSize(R.dimen.margin_space) + top,
-            0,
-            resources.getDimensionPixelSize(R.dimen.title_bar))
-
+        ion_view.setPadding(0, resources.getDimensionPixelSize(R.dimen.title_bar) + resources.getDimensionPixelSize(R.dimen.margin_space) + top, 0, resources.getDimensionPixelSize(R.dimen.title_bar))
         val params2 = common_title_back_ion.layoutParams as ViewGroup.LayoutParams
         params2.height = top + resources.getDimensionPixelSize(R.dimen.title_bar)
         common_title_back_ion.layoutParams = params2
@@ -74,40 +108,22 @@ class IonActivity : BaseActivity() {
     private fun recyclerView() {
         val recyclerView = findViewById<RecyclerView>(R.id.ion_view)
         val ionList = ArrayList<Ion>()
-
         IonModel.getList(ionList)
         recyclerView.layoutManager = LinearLayoutManager(this, RecyclerView.VERTICAL, false)
         val adapter = IonAdapter(ionList, this, this)
         recyclerView.adapter = adapter
-
         adapter.notifyDataSetChanged()
 
         edit_ion.addTextChangedListener(object : TextWatcher {
-            override fun beforeTextChanged(
-                s: CharSequence,
-                start: Int,
-                count: Int,
-                after: Int
-            ) {}
-            override fun onTextChanged(
-                s: CharSequence,
-                start: Int,
-                before: Int,
-                count: Int
-            ){}
-
-            override fun afterTextChanged(s: Editable) {
-                filter(s.toString(), ionList, recyclerView)
-            }
+            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int){}
+            override fun afterTextChanged(s: Editable) { filter(s.toString(), ionList, recyclerView) }
         })
     }
 
     private fun filter(text: String, list: ArrayList<Ion>, recyclerView: RecyclerView) {
         val filteredList: ArrayList<Ion> = ArrayList()
-        for (item in list) {
-            if (item.name.toLowerCase(Locale.ROOT).contains(text.toLowerCase(Locale.ROOT))) { filteredList.add(item) }
-
-        }
+        for (item in list) { if (item.name.toLowerCase(Locale.ROOT).contains(text.toLowerCase(Locale.ROOT))) { filteredList.add(item) } }
         mAdapter.filterList(filteredList)
         mAdapter.notifyDataSetChanged()
         recyclerView.adapter = IonAdapter(filteredList, this, this)
@@ -117,14 +133,12 @@ class IonActivity : BaseActivity() {
         search_btn_ion.setOnClickListener {
             Utils.fadeInAnim(search_bar_ion, 150)
             Utils.fadeOutAnim(title_box, 1)
-
             edit_ion.requestFocus()
             val imm: InputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
             imm.showSoftInput(edit_ion, InputMethodManager.SHOW_IMPLICIT)
         }
         close_ele_search_ion.setOnClickListener {
             Utils.fadeOutAnim(search_bar_ion, 1)
-
             val delayClose = Handler()
             delayClose.postDelayed({
                 Utils.fadeInAnim(title_box, 150)
@@ -138,6 +152,12 @@ class IonActivity : BaseActivity() {
         }
     }
 
+    override fun onBackPressed() {
+        if (ion_detail.visibility == View.VISIBLE) {
+            Utils.fadeOutAnim(ion_detail, 300)
+            return
+        } else { super.onBackPressed() }
+    }
 
 }
 
--- app/src/main/java/com/jlindemann/science/adapter/IonAdapter.kt
+++ app/src/main/java/com/jlindemann/science/adapter/IonAdapter.kt
@@ -4,11 +4,14 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.FrameLayout
+import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.jlindemann.science.R
 import com.jlindemann.science.activities.tables.IonActivity
+import com.jlindemann.science.model.Equation
 import com.jlindemann.science.model.Ion
 import org.json.JSONArray
 import org.json.JSONObject
@@ -18,25 +21,24 @@ import java.io.InputStream
 
 class IonAdapter(var list: ArrayList<Ion>, var clickListener: IonActivity, val context: Context) : RecyclerView.Adapter<IonAdapter.ViewHolder>() {
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.initialize(list[position], context)
+        holder.initialize(list[position], clickListener, context)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val v = LayoutInflater.from(parent.context).inflate(R.layout.electrode_list_item, parent, false)
+        val v = LayoutInflater.from(parent.context).inflate(R.layout.row_ions_list, parent, false)
         return ViewHolder(v)
     }
 
-    override fun getItemCount(): Int {
-        return list.size
-    }
+    override fun getItemCount(): Int { return list.size }
 
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        private val textViewName = itemView.findViewById(R.id.tv_name) as TextView
-        private val textViewShort = itemView.findViewById(R.id.tv_short) as TextView
-        private val textViewCharge = itemView.findViewById(R.id.tv_charge) as TextView
-        private val textViewVoltage = itemView.findViewById(R.id.tv_voltage) as TextView
+        private val cardView = itemView.findViewById(R.id.ionCard) as FrameLayout
+        private val textViewName = itemView.findViewById(R.id.tv_name_d) as TextView
+        private val textViewShort = itemView.findViewById(R.id.tv_short_d) as TextView
+        private val textViewCharge = itemView.findViewById(R.id.tv_end) as TextView
+        private val textViewVoltage = itemView.findViewById(R.id.tv_ionization) as TextView
 
-        fun initialize(item: Ion, context: Context) {
+        fun initialize(item: Ion, action: OnIonClickListener, context: Context) {
             var jsonString : String? = null
             try {
                 val ext = ".json"
@@ -52,16 +54,20 @@ class IonAdapter(var list: ArrayList<Ion>, var clickListener: IonActivity, val c
                 val ionization1 = jsonObject.optString("element_ionization_energy1", "---")
                 textViewVoltage.text = ionization1
 
+
             }
             catch (e: IOException) { }
             textViewName.text = item.name
             textViewName.text = item.name.capitalize()
             textViewShort.text = item.short
-            textViewCharge.text = item.count.toString()
+            textViewCharge.text = "View all" + " " + item.count.toString()
 
-            itemView.foreground = ContextCompat.getDrawable(context, R.drawable.c_ripple)
-            itemView.isClickable = true
-            itemView.isFocusable = true
+            cardView.foreground = ContextCompat.getDrawable(context, R.drawable.c_ripple)
+            cardView.isClickable = true
+            cardView.isFocusable = true
+            cardView.setOnClickListener {
+                action.ionClickListener(item, adapterPosition)
+            }
 
         }
     }
@@ -71,7 +77,9 @@ class IonAdapter(var list: ArrayList<Ion>, var clickListener: IonActivity, val c
         notifyDataSetChanged()
 
     }
-
+    interface OnIonClickListener {
+        fun ionClickListener(item: Ion, position: Int)
+    }
 }
 
 
