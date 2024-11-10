4d1f46c6b53cd39d690add394e7025987d887870
Jonatan Lindemann <46776594+JLindemann42@users.noreply.github.com>
2024-11-10 12:08:59 +0100

    -

--- app/src/main/java/com/jlindemann/science/model/Constants.kt
+++ app/src/main/java/com/jlindemann/science/model/Constants.kt
@@ -1,4 +1,4 @@
 package com.jlindemann.science.model
 
-data class Constants(val name: String, val value: Double, val unit: String, val info: String, val category: String)
+data class Constants(val name: String, val value: String, val unit: String, val info: String, val category: String)
 
