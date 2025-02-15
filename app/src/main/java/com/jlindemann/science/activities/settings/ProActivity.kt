1eadbde728604efeb85af42eeba2276c5c9edc3a
Jonatan Lindemann <46776594+JLindemann42@users.noreply.github.com>
2025-02-15 23:08:43 +0100

    Code for Sources Info Page

--- app/src/main/java/com/jlindemann/science/activities/settings/ProActivity.kt
+++ app/src/main/java/com/jlindemann/science/activities/settings/ProActivity.kt
@@ -23,19 +23,7 @@ import android.widget.Toast
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.content.ContextCompat
 import androidx.lifecycle.lifecycleScope
-import com.android.billingclient.api.AcknowledgePurchaseParams
-import com.android.billingclient.api.BillingClient
-import com.android.billingclient.api.BillingClientStateListener
-import com.android.billingclient.api.BillingFlowParams
-import com.android.billingclient.api.BillingResult
-import com.android.billingclient.api.ProductDetails
-import com.android.billingclient.api.Purchase
-import com.android.billingclient.api.PurchasesUpdatedListener
-import com.android.billingclient.api.QueryProductDetailsParams
-import com.android.billingclient.api.QueryPurchasesParams
-import com.android.billingclient.api.acknowledgePurchase
-import com.android.billingclient.api.queryProductDetails
-import com.android.billingclient.api.queryPurchasesAsync
+
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.jlindemann.science.R
 import com.jlindemann.science.activities.BaseActivity
@@ -59,12 +47,10 @@ import java.util.concurrent.Executors
 import kotlin.concurrent.schedule
 
 
-class ProActivity : BaseActivity(), BillingClientStateListener {
+class ProActivity : BaseActivity() {
 
     val NON_CONSUMALBE_COUNTER_ID = "pro_version"
 
-    private var billingClient: BillingClient? = null
-    private var productDetailList = mutableListOf<ProductDetails>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -73,271 +59,25 @@ class ProActivity : BaseActivity(), BillingClientStateListener {
 
         if (themePrefValue == 100) {
             when (resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK) {
-                Configuration.UI_MODE_NIGHT_NO -> { setTheme(R.style.AppTheme) }
-                Configuration.UI_MODE_NIGHT_YES -> { setTheme(R.style.AppThemeDark) }
+                Configuration.UI_MODE_NIGHT_NO -> {
+                    setTheme(R.style.AppTheme)
+                }
+
+                Configuration.UI_MODE_NIGHT_YES -> {
+                    setTheme(R.style.AppThemeDark)
+                }
             }
         }
-        if (themePrefValue == 0) { setTheme(R.style.AppTheme) }
-        if (themePrefValue == 1) { setTheme(R.style.AppThemeDark) }
+        if (themePrefValue == 0) {
+            setTheme(R.style.AppTheme)
+        }
+        if (themePrefValue == 1) {
+            setTheme(R.style.AppThemeDark)
+        }
         setContentView(R.layout.activity_pro_v2)
-        findViewById<FrameLayout>(R.id.view_pro).systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+        findViewById<FrameLayout>(R.id.view_pro).systemUiVisibility =
+            View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
 
-        checkSale()
-
-        findViewById<ImageButton>(R.id.back_btn_pro).setOnClickListener {
-            this.onBackPressed()
-        }
-        findViewById<TextView>(R.id.buy_btn).setOnClickListener {
-            try {
-                if (productDetailList.isEmpty()) {
-                    ToastUtil.showToast(this, "No products found")
-                }
-                else {
-                    launchBillingFlow(productDetailList[0])
-                }
-            }
-            catch (e: IOException) {
-                ToastUtil.showToast(this, "Try again")
-            }
-        }
-        findViewById<TextView>(R.id.product_text).setOnClickListener {
-            try {
-                if (productDetailList.isEmpty()) {
-                    ToastUtil.showToast(this, "No products found")
-                }
-                else {
-                    launchBillingFlow(productDetailList[0])
-                }
-            }
-            catch (e: IOException) {
-                ToastUtil.showToast(this, "Try again")
-            }
-        }
-        val purchasesUpdateListener =
-            PurchasesUpdatedListener { billingResult, purchases ->
-                when (billingResult.responseCode) {
-                    BillingClient.BillingResponseCode.OK -> {
-                        if (purchases?.isNotEmpty() == true) {
-                            for (purchase in purchases) {
-                                lifecycleScope.launch {
-                                    val jsonString = purchase.originalJson
-                                    try {
-                                        val jsonObject = JSONObject(jsonString)
-                                        val productId = jsonObject.getString("productId")
-                                        if (productId == NON_CONSUMALBE_COUNTER_ID) {
-                                            acknowledgePurchase(purchase)
-                                        }
-                                    } catch (e: Exception) {
-                                        println()
-                                        ToastUtil.showToast(this@ProActivity, "Error parsing JSON")
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-        }
-        initBillingClient(purchasesUpdateListener)
-    }
-
-    private fun initBillingClient(purschasesUpdateListener: PurchasesUpdatedListener) {
-        billingClient = BillingClient.newBuilder(this)
-            .setListener(purschasesUpdateListener)
-            .enablePendingPurchases()
-            .build()
-        billingClient?.startConnection(this)
-    }
-
-    override fun onBillingServiceDisconnected() {
-        if (billingClient?.isReady == true) {
-            billingClient?.startConnection(this)
-        }
-    }
-
-    override fun onBillingSetupFinished(billingResult: BillingResult) {
-        val responseCode = billingResult.responseCode
-        if (responseCode == BillingClient.BillingResponseCode.OK) {
-            queryProducts()
-            queryPurchases()
-        } else {
-            ToastUtil.showToast(this, responseCode.toString())
-        }
-    }
-
-    private fun queryProducts() {
-        lifecycleScope.launch {
-            queryProducts(BillingClient.ProductType.INAPP, NON_CONSUMALBE_COUNTER_ID)
-        }
-    }
-
-    private suspend fun queryProducts(productType: String, product: String) {
-        val queryProductDetailsParams =
-            QueryProductDetailsParams.newBuilder()
-                .setProductList(
-                    listOf(
-                        QueryProductDetailsParams.Product.newBuilder()
-                            .setProductId(product)
-                            .setProductType(productType)
-                            .build()
-                    )
-                )
-                .build()
-        val productDetailsResult = withContext(Dispatchers.IO) {
-            billingClient?.queryProductDetails(queryProductDetailsParams)
-        }
-
-        when (val responseCode = productDetailsResult?.billingResult?.responseCode) {
-            BillingClient.BillingResponseCode.OK -> {
-                if (productDetailsResult.productDetailsList?.isNotEmpty() == true) {
-                    productDetailList.addAll(productDetailsResult.productDetailsList!!)
-                }
-            }
-        }
-    }
-
-    private fun queryPurchases() {
-        queryPurchases(BillingClient.ProductType.INAPP)
-    }
-
-    private fun queryPurchases(type: String) {
-        if (billingClient?.isReady == false) {
-            ToastUtil.showToast(this, "Billing client not ready")
-            return
-        }
-        val queryPurchaseParams = QueryPurchasesParams.newBuilder()
-            .setProductType(type)
-            .build()
-
-        billingClient?.queryPurchasesAsync(queryPurchaseParams) { billingResult, productDetailList ->
-            when (val responseCode = billingResult.responseCode) {
-                BillingClient.BillingResponseCode.OK -> {
-                    if (productDetailList.isNotEmpty()) {
-                        for (purchase in productDetailList) {
-                            lifecycleScope.launch {
-                                val jsonString = purchase.originalJson
-                                try {
-                                    val jsonObject = JSONObject(jsonString)
-                                    val productId = jsonObject.getString("productId")
-                                    if (productId == NON_CONSUMALBE_COUNTER_ID)
-                                        acknowledgePurchase(purchase)
-                                } catch (e: java.lang.Exception){
-                                    ToastUtil.showToast(this@ProActivity, e.message.toString())
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    }
-
-    private suspend fun acknowledgePurchase(purchase: Purchase) {
-        if (purchase.purchaseState == Purchase.PurchaseState.PURCHASED) {
-            if (!purchase.isAcknowledged) {
-                val acknowledgePurchaseParams = AcknowledgePurchaseParams.newBuilder()
-                    .setPurchaseToken(purchase.purchaseToken)
-                withContext(Dispatchers.IO) {
-                    billingClient?.acknowledgePurchase(acknowledgePurchaseParams.build())
-                    { billingResult ->
-                        when (val responseCode = billingResult.responseCode) {
-                            BillingClient.BillingResponseCode.OK -> {
-                                showMemberInfo() //purschase accepted
-                            }
-                            else -> {
-                                ToastUtil.showToast(this@ProActivity, "Error")
-                            }
-                        }
-
-                    }
-                }
-            }
-            else {
-                showMemberInfo()
-            }
-        }
-    }
-
-    private fun checkSale() {
-        val saleStartDate = SimpleDateFormat("yyyy/MM/dd").parse(getString(R.string.next_sale_start)) //Back to school sale
-        val saleEndDate = SimpleDateFormat("yyyy/MM/dd").parse(getString(R.string.next_sale_end)) //Back to school sale
-        val calendar = Calendar.getInstance(TimeZone.getDefault())
-        val date = calendar.time
-        val proText = findViewById<TextView>(R.id.pro_price)
-        val proDiscText = findViewById<TextView>(R.id.pro_price_discount)
-
-        if (date > saleStartDate) {
-            //Set new attributes for DonateBtn
-            proText.text = "1.79 USD"
-            proText.setTextColor(getColorStateList(R.color.orange))
-            proDiscText.visibility = View.VISIBLE
-            proDiscText.paintFlags = Paint.STRIKE_THRU_TEXT_FLAG
-        }
-
-        if (date > saleEndDate) {
-            Timer().schedule(2) {
-                proText.text = "1.99 USD"
-                proDiscText.visibility = View.GONE
-            }
-        }
-        else {
-        }
-    }
-
-    private fun launchBillingFlow(productDetails: ProductDetails) {
-        try {
-            val productDetailsParamsList =
-                listOf(
-                    BillingFlowParams.ProductDetailsParams.newBuilder()
-                        .setProductDetails(productDetails)
-                        .build()
-                )
-            val billingFlowParams = BillingFlowParams.newBuilder()
-                .setProductDetailsParamsList(productDetailsParamsList)
-                .build()
-            if (billingClient?.isReady == false) {
-                ToastUtil.showToast(this, "Billing client not ready, try again!")
-            }
-            val billingResult = billingClient?.launchBillingFlow(this, billingFlowParams)
-
-            when (val responseCode = billingResult?.responseCode) {
-                BillingClient.BillingResponseCode.OK -> {
-                    val proPref = ProVersion(this@ProActivity)
-                    var proPrefValue = proPref.getValue()
-                }
-        }
-        }
-        catch (e: IOException) {
-            ToastUtil.showToast(this, "Try again")
-        }
-    }
-
-    private fun showMemberInfo() {
-        val proPref = ProVersion(this@ProActivity)
-        var proPrefValue = proPref.getValue()
-        proPref.setValue(100)
-        findViewById<TextView>(R.id.pro_title_view).text = "You are getting more out of Atomic"
-        val drawable = ContextCompat.getDrawable(this, R.drawable.ic_lock_open)
-        findViewById<TextView>(R.id.pro_data_lock).setCompoundDrawablesWithIntrinsicBounds(drawable, null, null, null)
-        findViewById<TextView>(R.id.pro_data_lock).text = "Unlocked"
-        findViewById<TextView>(R.id.pro_table_lock).setCompoundDrawablesWithIntrinsicBounds(drawable, null, null, null)
-        findViewById<TextView>(R.id.pro_table_lock).text = "Unlocked"
-        findViewById<TextView>(R.id.pro_visualisation_lock).setCompoundDrawablesWithIntrinsicBounds(drawable, null, null, null)
-        findViewById<TextView>(R.id.pro_visualisation_lock).text = "Unlocked"
-        findViewById<FrameLayout>(R.id.pro_purschase_box).visibility = View.GONE
-        findViewById<TextView>(R.id.product_title).visibility = View.GONE
-        findViewById<TextView>(R.id.product_text).visibility = View.GONE
-    }
-
-    override fun onApplySystemInsets(top: Int, bottom: Int, left: Int, right: Int) {
-            val params = findViewById<FrameLayout>(R.id.common_title_back_pro).layoutParams as ViewGroup.LayoutParams
-            params.height = top + resources.getDimensionPixelSize(R.dimen.title_bar)
-            findViewById<FrameLayout>(R.id.common_title_back_pro).layoutParams = params
-
-            findViewById<LinearLayout>(R.id.pro_linear).setPadding(0, top, 0, 0)
-
-            val params2 = findViewById<FrameLayout>(R.id.pro_purschase_box).layoutParams as ViewGroup.LayoutParams
-            params2.height = bottom + resources.getDimensionPixelSize(R.dimen.nav_bar)
-            findViewById<FrameLayout>(R.id.pro_purschase_box).layoutParams = params2
 
     }
 
---
+++ app/src/main/java/com/jlindemann/science/activities/settings/SourcesActivity.kt
@@ -0,0 +1,112 @@
+package com.jlindemann.science.activities.settings
+
+import android.content.res.Configuration
+import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
+import android.view.ViewTreeObserver
+import android.widget.CheckBox
+import android.widget.FrameLayout
+import android.widget.ImageButton
+import android.widget.ScrollView
+import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.jlindemann.science.R
+import com.jlindemann.science.activities.BaseActivity
+import com.jlindemann.science.animations.Anim
+import com.jlindemann.science.preferences.ThemePreference
+
+class SourcesActivity : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val themePreference = ThemePreference(this)
+        val themePrefValue = themePreference.getValue()
+
+        if (themePrefValue == 100) {
+            when (resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK) {
+                Configuration.UI_MODE_NIGHT_NO -> { setTheme(R.style.AppTheme) }
+                Configuration.UI_MODE_NIGHT_YES -> { setTheme(R.style.AppThemeDark) }
+            }
+        }
+        if (themePrefValue == 0) { setTheme(R.style.AppTheme) }
+        if (themePrefValue == 1) { setTheme(R.style.AppThemeDark) }
+        setContentView(R.layout.activity_settings_licenses) //REMEMBER: Never move any function calls above this
+
+        findViewById<FrameLayout>(R.id.view_src).systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+
+        //Title Controller
+        findViewById<FrameLayout>(R.id.common_title_back_src_color).visibility = View.INVISIBLE
+        findViewById<TextView>(R.id.sources_title).visibility = View.INVISIBLE
+        findViewById<FrameLayout>(R.id.common_title_back_src).elevation = (resources.getDimension(R.dimen.zero_elevation))
+        findViewById<ScrollView>(R.id.sources_scroll).getViewTreeObserver()
+            .addOnScrollChangedListener(object : ViewTreeObserver.OnScrollChangedListener {
+                var y = 300f
+                override fun onScrollChanged() {
+                    if (findViewById<ScrollView>(R.id.sources_scroll).getScrollY() > 150) {
+                        findViewById<FrameLayout>(R.id.common_title_back_src_color).visibility = View.VISIBLE
+                        findViewById<TextView>(R.id.sources_title).visibility = View.VISIBLE
+                        findViewById<TextView>(R.id.sources_title_downstate).visibility = View.INVISIBLE
+                        findViewById<FrameLayout>(R.id.common_title_back_src).elevation = (resources.getDimension(R.dimen.one_elevation))
+                    } else {
+                        findViewById<FrameLayout>(R.id.common_title_back_src_color).visibility = View.INVISIBLE
+                        findViewById<TextView>(R.id.sources_title).visibility = View.INVISIBLE
+                        findViewById<TextView>(R.id.sources_title_downstate).visibility = View.VISIBLE
+                        findViewById<FrameLayout>(R.id.common_title_back_src).elevation = (resources.getDimension(R.dimen.zero_elevation))
+                    }
+                    y = findViewById<ScrollView>(R.id.sources_scroll).getScrollY().toFloat()
+                }
+            })
+
+        listeners()
+        findViewById<ImageButton>(R.id.back_btn_src).setOnClickListener {
+            this.onBackPressed()
+        }
+    }
+
+    override fun onApplySystemInsets(top: Int, bottom: Int, left: Int, right: Int) {
+        val params2 = findViewById<FrameLayout>(R.id.common_title_back_src).layoutParams as ViewGroup.LayoutParams
+        params2.height = top + resources.getDimensionPixelSize(R.dimen.title_bar)
+        findViewById<FrameLayout>(R.id.common_title_back_src).layoutParams = params2
+
+        val params3 = findViewById<TextView>(R.id.sources_title_downstate).layoutParams as ViewGroup.MarginLayoutParams
+        params3.topMargin = top + resources.getDimensionPixelSize(R.dimen.title_bar) + resources.getDimensionPixelSize(R.dimen.header_down_margin)
+        findViewById<TextView>(R.id.sources_title_downstate).layoutParams = params3
+    }
+
+    override fun onBackPressed() {
+        if (findViewById<ConstraintLayout>(R.id.l_inc).visibility == View.VISIBLE) {
+            hideInfoPanel()
+            return
+        } else { super.onBackPressed() }
+    }
+
+    private fun listeners() {
+        findViewById<FrameLayout>(R.id.l_wwe_btn).setOnClickListener {
+            val title = resources.getString(R.string.sources_wwe_title)
+            val text = resources.getString(R.string.sources_wwe_text)
+            showInfoPanel(title, text)
+        }
+        findViewById<FrameLayout>(R.id.l_sothree_btn).setOnClickListener {
+            val title = resources.getString(R.string.sothree_license)
+            val text = resources.getString(R.string.sothree_license_text)
+            showInfoPanel(title, text)
+        }
+
+        findViewById<FloatingActionButton>(R.id.l_back_btn).setOnClickListener { hideInfoPanel() }
+        findViewById<TextView>(R.id.l_background3).setOnClickListener { hideInfoPanel() }
+    }
+
+    private fun showInfoPanel(title: String, text: String) {
+        Anim.fadeIn(findViewById<ConstraintLayout>(R.id.l_inc), 150)
+        findViewById<TextView>(R.id.l_title).text = title
+        findViewById<TextView>(R.id.l_text).text = text
+    }
+
+    private fun hideInfoPanel() {
+        Anim.fadeOutAnim(findViewById<ConstraintLayout>(R.id.l_inc), 150)
+    }
+}
+
+
+
