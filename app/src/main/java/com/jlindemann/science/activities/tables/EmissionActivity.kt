a3c98bd421636c9b732686b99336d3e5d8dfd148
Jonatan Lindemann <46776594+JLindemann42@users.noreply.github.com>
2025-03-16 01:27:36 +0100

    -

--- app/src/main/java/com/jlindemann/science/activities/tables/EmissionActivity.kt
+++ app/src/main/java/com/jlindemann/science/activities/tables/EmissionActivity.kt
@@ -12,12 +12,14 @@ import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
 import android.widget.FrameLayout
 import android.widget.ImageButton
+import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.jlindemann.science.R
 import com.jlindemann.science.activities.BaseActivity
+import com.jlindemann.science.adapter.ElementAdapter
 import com.jlindemann.science.adapter.EmissionAdapter
 import com.jlindemann.science.adapter.EquationsAdapter
 import com.jlindemann.science.adapter.IonAdapter
@@ -30,10 +32,13 @@ import com.jlindemann.science.model.IonModel
 import com.jlindemann.science.preferences.MostUsedPreference
 import com.jlindemann.science.preferences.ThemePreference
 import com.jlindemann.science.utils.Utils
+import com.sothree.slidinguppanel.SlidingUpPanelLayout
+import com.squareup.picasso.Picasso
 import org.json.JSONArray
 import org.json.JSONObject
 import java.io.IOException
 import java.io.InputStream
+import java.net.ConnectException
 import java.util.*
 import kotlin.collections.ArrayList
 
@@ -69,12 +74,47 @@ class EmissionActivity : BaseActivity(), EmissionAdapter.OnEmissionClickListener
 
         recyclerView()
         clickSearch()
+
+        findViewById<SlidingUpPanelLayout>(R.id.sliding_layout_e).addPanelSlideListener(object : SlidingUpPanelLayout.PanelSlideListener {
+            override fun onPanelSlide(panel: View?, slideOffset: Float) { }
+            override fun onPanelStateChanged(panel: View?, previousState: SlidingUpPanelLayout.PanelState, newState: SlidingUpPanelLayout.PanelState) {
+                if (findViewById<SlidingUpPanelLayout>(R.id.sliding_layout_e).panelState === SlidingUpPanelLayout.PanelState.COLLAPSED) {
+                    Utils.fadeOutAnim(findViewById<FrameLayout>(R.id.emission_detail), 300)
+                }
+            }
+        })
+
         findViewById<FrameLayout>(R.id.view_emi).systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
         findViewById<ImageButton>(R.id.back_btn_emi).setOnClickListener { this.onBackPressed() }
     }
 
     override fun emiClickListener(item: Element, position: Int) {
+        var jsonString : String? = null
+        try {
+            val ext = ".json"
+            val element = item.element
+            val elementJson: String? = "$element$ext"
+            val inputStream: InputStream = assets.open(elementJson.toString())
+            jsonString = inputStream.bufferedReader().use { it.readText() }
+            val jsonArray = JSONArray(jsonString)
+            val jsonObject: JSONObject = jsonArray.getJSONObject(0)
 
+            val url = jsonObject.optString("short", "---")
+            val hUrl = "https://www.jlindemann.se/atomic/emission_lines/"
+            val extg = ".gif"
+            val fURL = hUrl + url + extg
+            try {
+                Picasso.get().load(fURL).into(findViewById<ImageView>(R.id.emi_img_detail))
+            }
+            catch(e: ConnectException) {
+                //findViewById<ImageView>(R.id.sp_img).visibility = View.GONE
+                //findViewById<TextView>(R.id.sp_offline).text = "No Data"
+                //findViewById<TextView>(R.id.sp_offline).visibility = View.VISIBLE
+            }
+        }
+        catch (e: IOException) { }
+        Utils.fadeInAnim(findViewById<FrameLayout>(R.id.emission_detail), 300)
+        findViewById<SlidingUpPanelLayout>(R.id.sliding_layout_e).panelState = SlidingUpPanelLayout.PanelState.EXPANDED
     }
 
     private fun recyclerView() {
@@ -86,40 +126,40 @@ class EmissionActivity : BaseActivity(), EmissionAdapter.OnEmissionClickListener
         recyclerView.adapter = adapter
         adapter.notifyDataSetChanged()
 
-        findViewById<EditText>(R.id.edit_ion).addTextChangedListener(object : TextWatcher {
+        findViewById<EditText>(R.id.edit_emi).addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
             override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int){}
-            override fun afterTextChanged(s: Editable) { filter(s.toString(), ionList, recyclerView) }
+            override fun afterTextChanged(s: Editable) { filter(s.toString(), emiList, recyclerView) }
         })
     }
 
-    private fun filter(text: String, list: ArrayList<Ion>, recyclerView: RecyclerView) {
-        val filteredList: ArrayList<Ion> = ArrayList()
-        for (item in list) { if (item.name.toLowerCase(Locale.ROOT).contains(text.toLowerCase(Locale.ROOT))) { filteredList.add(item) } }
+    private fun filter(text: String, list: ArrayList<Element>, recyclerView: RecyclerView) {
+        val filteredList: ArrayList<Element> = ArrayList()
+        for (item in list) { if (item.element.toLowerCase(Locale.ROOT).contains(text.toLowerCase(Locale.ROOT))) { filteredList.add(item) } }
         val handler = android.os.Handler()
         handler.postDelayed({
             if (recyclerView.adapter!!.itemCount == 0) {
-                Anim.fadeIn(findViewById<LinearLayout>(R.id.empty_search_box_ion), 300)
+                Anim.fadeIn(findViewById<LinearLayout>(R.id.empty_search_box_emi), 300)
             }
             else {
-                findViewById<LinearLayout>(R.id.empty_search_box_ion).visibility = View.GONE
+                findViewById<LinearLayout>(R.id.empty_search_box_emi).visibility = View.GONE
             }
         }, 10)
         mAdapter.filterList(filteredList)
         mAdapter.notifyDataSetChanged()
-        recyclerView.adapter = IonAdapter(filteredList, this, this)
+        recyclerView.adapter = EmissionAdapter(filteredList, this, this)
     }
 
     private fun clickSearch() {
-        findViewById<ImageButton>(R.id.search_btn_ion).setOnClickListener {
-            Utils.fadeInAnim(findViewById<FrameLayout>(R.id.search_bar_ion), 150)
+        findViewById<ImageButton>(R.id.search_btn_emi).setOnClickListener {
+            Utils.fadeInAnim(findViewById<FrameLayout>(R.id.search_bar_emi), 150)
             Utils.fadeOutAnim(findViewById<FrameLayout>(R.id.title_box), 1)
-            findViewById<EditText>(R.id.edit_ion).requestFocus()
+            findViewById<EditText>(R.id.edit_emi).requestFocus()
             val imm: InputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-            imm.showSoftInput(findViewById<EditText>(R.id.edit_ion), InputMethodManager.SHOW_IMPLICIT)
+            imm.showSoftInput(findViewById<EditText>(R.id.edit_emi), InputMethodManager.SHOW_IMPLICIT)
         }
-        findViewById<ImageButton>(R.id.close_ele_search_ion).setOnClickListener {
-            Utils.fadeOutAnim(findViewById<FrameLayout>(R.id.search_bar_ion), 1)
+        findViewById<ImageButton>(R.id.close_ele_search_emi).setOnClickListener {
+            Utils.fadeOutAnim(findViewById<FrameLayout>(R.id.search_bar_emi), 1)
             val delayClose = Handler()
             delayClose.postDelayed({
                 Utils.fadeInAnim(findViewById<FrameLayout>(R.id.title_box), 150)
@@ -134,22 +174,23 @@ class EmissionActivity : BaseActivity(), EmissionAdapter.OnEmissionClickListener
     }
 
     override fun onBackPressed() {
-        if (findViewById<FrameLayout>(R.id.ion_detail).visibility == View.VISIBLE) {
-            Utils.fadeOutAnim(findViewById<FrameLayout>(R.id.ion_detail), 300)
-            return
-        } else { super.onBackPressed() }
+        super.onBackPressed()
     }
 
     override fun onApplySystemInsets(top: Int, bottom: Int, left: Int, right: Int) {
-        findViewById<RecyclerView>(R.id.ion_view).setPadding(0, resources.getDimensionPixelSize(R.dimen.title_bar) + resources.getDimensionPixelSize(R.dimen.margin_space) + top, 0, resources.getDimensionPixelSize(R.dimen.title_bar))
+        findViewById<RecyclerView>(R.id.emi_view).setPadding(0, resources.getDimensionPixelSize(R.dimen.title_bar) + resources.getDimensionPixelSize(R.dimen.margin_space) + top, 0, resources.getDimensionPixelSize(R.dimen.title_bar))
 
-        val params2 = findViewById<FrameLayout>(R.id.common_title_back_ion).layoutParams as ViewGroup.LayoutParams
+        val params2 = findViewById<FrameLayout>(R.id.common_title_back_emi).layoutParams as ViewGroup.LayoutParams
         params2.height = top + resources.getDimensionPixelSize(R.dimen.title_bar)
-        findViewById<FrameLayout>(R.id.common_title_back_ion).layoutParams = params2
+        findViewById<FrameLayout>(R.id.common_title_back_emi).layoutParams = params2
 
-        val searchEmptyImgPrm = findViewById<LinearLayout>(R.id.empty_search_box_ion).layoutParams as ViewGroup.MarginLayoutParams
+        val searchEmptyImgPrm = findViewById<LinearLayout>(R.id.empty_search_box_emi).layoutParams as ViewGroup.MarginLayoutParams
         searchEmptyImgPrm.topMargin = top + (resources.getDimensionPixelSize(R.dimen.title_bar))
-        findViewById<LinearLayout>(R.id.empty_search_box_ion).layoutParams = searchEmptyImgPrm
+        findViewById<LinearLayout>(R.id.empty_search_box_emi).layoutParams = searchEmptyImgPrm
+
+        val params3 = findViewById<SlidingUpPanelLayout>(R.id.sliding_layout_e).layoutParams as ViewGroup.MarginLayoutParams
+        params3.topMargin = top + resources.getDimensionPixelSize(R.dimen.panel_margin)
+        findViewById<SlidingUpPanelLayout>(R.id.sliding_layout_e).layoutParams = params3
     }
 
 }
