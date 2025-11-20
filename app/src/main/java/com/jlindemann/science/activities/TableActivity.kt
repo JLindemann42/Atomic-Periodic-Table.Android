f509ae5cd9a303729bd404b8d1cbf22f8be2475b
Jonatan Lindemann <46776594+JLindemann42@users.noreply.github.com>
2025-11-20 12:53:29 +0100

    -

--- app/src/main/java/com/jlindemann/science/activities/TableActivity.kt
+++ app/src/main/java/com/jlindemann/science/activities/TableActivity.kt
@@ -5,8 +5,8 @@ import android.content.res.Configuration
 import android.os.Bundle
 import android.view.View
 import android.view.ViewGroup
-import android:view.ViewTreeObserver
-import android:widget.FrameLayout
+import android.view.ViewTreeObserver
+import android.widget.FrameLayout
 import android.widget.ImageButton
 import android.widget.ScrollView
 import android.widget.TextView
